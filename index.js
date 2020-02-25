a12150cab7223b7fdeadbdc62d27968309e931a9
Denner Marchi <denner883@hotmail.com>
2020-02-25 16:19:07 -0300

    correção de bug no /buscar

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const url = require('url');
 const petshop = require("./petshop");
 
 const server = http.createServer((req, res) => {
-	res.writeHead(200, {"Content-Type": "text/plain; charset=UTF-8"});
+	res.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
 	let urlCompleta = url.parse(req.url, true);
 	let queryString = urlCompleta.query;
 	let rota = urlCompleta.pathname;
@@ -16,31 +16,36 @@ const server = http.createServer((req, res) => {
 		let novoPet = queryString;
 
 		if (petshop.adicionarPet(novoPet)) {
-			res.write(`${novoPet.nome} foi cadastrado com sucesso!`);
+			res.write(`<p>${novoPet.nome} foi cadastrado com sucesso!</p>`);
 			console.log(petshop.pets);
 		} else {
-			res.write("Ops, algo deu errado!");
+			res.write("<p>Ops, algo deu errado!</p>");
 		}
 		break;
 
 	case "/buscar":
+		if (!queryString.nome) {
+			res.write("<p>Por favor informe o nome do pet que deseja procurar!</p>")
+			break;
+		}
+
 		let petsEncontrados = petshop.buscarPet(queryString.nome);
 
 		if (petsEncontrados.length > 0) {
-			res.write(`Encontramos ${petsEncontrados.length} pet(s) com o nome ${queryString.nome}`)
+			res.write(`<p>Encontramos ${petsEncontrados.length} pet(s) com o nome ${queryString.nome}</p>`)
 		} else {
-			res.write("Ops, não encontramos nenhum pet com esse nome!");
+			res.write("<p>Ops, não encontramos nenhum pet com esse nome!</p>");
 		}
 		break;
 
 	default:
-		res.write("** Bem vindos ao Petshop **");
+		res.write("<h1>** Bem vindos ao Petshop **</h1>");
 
 		const pets = petshop.listarPets();
 
 		pets.length > 0
 				? res.write(pets) 
-				: res.write("Nenhum pet cadastrado :(");
+				: res.write("<p>Nenhum pet cadastrado :(</p>");
 		break;
 	}
 	res.end();
